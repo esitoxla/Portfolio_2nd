df0d83b977c036dc64568e5dc3f238cc7c99cbdd
Irene Toxla <toxlairene@gmail.com>
2024-12-06 23:12:34 +0000

    added final changes in css

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ const menuToggle = document.querySelector(".menu-toggle");
 const navItems = document.querySelector(".nav-items");
 
 // Add click event to toggle the menu
-menuToggle.addEventListener("click", () => {
-  navItems.classList.toggle("active"); // Toggle active class
-  
-});
+if (menuToggle && navItems) {
+  menuToggle.addEventListener("click", () => {
+    console.log("Menu toggle clicked"); // Debug log
+    navItems.classList.toggle("active");
+    console.log(navItems.classList);
+  });
+} else {
+  console.log("manu toggle or nav items not found");
+}
